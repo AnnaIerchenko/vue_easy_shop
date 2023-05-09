c2452026cac1e2da41185940dc97b5b5cf4e138a
AnnaIerchenko <79634675+AnnaIerchenko@users.noreply.github.com>
2023-05-09 11:08:28 -0700

    modal window with info about product, add item to cart from modal popup

--- src/components/catalog/CatalogItem.vue
+++ src/components/catalog/CatalogItem.vue
@@ -1,5 +1,27 @@
 <template>
   <div class="catalog-item">
+    <catalog-popup
+      v-if="isPopupVisible"
+      rightBtnPopup="Add to Cart"
+      :popup-title="product_data.name"
+      @closePopup="closePopup"
+      @rightBtnAction="addToCart"
+    >
+    <div class="catalog-item__popup">
+    <div class="catalog-item__image-wrap">
+    <img 
+      class="catalog-item__image"
+      :src="require('../../assets/images/' + product_data.image)"
+      alt="img"      
+    />
+    </div>
+      <div class="catalog-item__content">
+      <p class="catalog-item__name">{{ product_data.name }}</p>
+      <p class="catalog-item__price">Price: {{ product_data.price }} $</p>
+      <p class="catalog-item__price">Price: {{ product_data.category }} </p>
+      </div>
+    </div>
+    </catalog-popup>
     <div class="catalog-item__image-wrap">
     <img 
       class="catalog-item__image"
@@ -9,18 +31,33 @@
   </div>
     <p class="catalog-item__name">{{ product_data.name }}</p>
     <p class="catalog-item__price">Price: {{ product_data.price }} $</p>
+    <div class="catalog-item__btns">
+    <button
+      class="catalog-item__show-info btn"
+      @click="showPopup"
+    >Show info</button>
     <button 
       class="catalog-item__btn btn"
       @click="addToCart"
     >
       Add to cart</button>
+    </div>
   </div>
 </template>
 
 <script>
+import CatalogPopup from "../popup/CatalogPopup.vue"
+
   export default {
     name: 'CatalogItem',
-    components: {},
+    components: {
+      CatalogPopup
+    },
+    data(){
+      return {
+        isPopupVisible: false
+      }
+    },
     props: {
       product_data: {
         type: Object,
@@ -32,7 +69,13 @@
     methods: {
       addToCart() {
         this.$emit('addToCart', this.product_data)
-      }
+      },
+      showPopup() {
+        this.isPopupVisible = true
+      },
+      closePopup() {
+        this.isPopupVisible = false
+      },
     }
   }
 </script>
@@ -59,5 +102,24 @@
     width: 100%;
     height: 100%;
   }
- }
+  &__show-info.btn{
+    background-color: $main-color;
+  }
+  &__btns {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: stretch;
+    margin: 0 $margin*3;
+    gap: $padding;
+  }
+  &__popup {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  }
+  &__content {
+    padding-left: $padding;
+  }
+}
 </style>
